9851e3f0b1ef9959c030962e12a22142450095c7
Cassiano Magno Chagas <cassianomagnochagas@gmail.com>
2026-05-22 18:35:23 -0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import './Layout.css';
+import './AuthModal.css';
 import logo from '../img/candleliticsTextLogo.png';
+import AuthModal from './AuthModal';
 
 function Layout() {
+
+  const [modalType, setModalType] = useState(null);
+
   return (
     <div>
       <nav className="navbar">
@@ -11,11 +16,20 @@ function Layout() {
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/sobre" className="nav-link">Sobre</Link>
         <Link to="/dashboards" className="nav-link">Meu Dashboard</Link>
+        
+        <div className="auth-buttons">
+          <button className="btn-login" onClick={() => setModalType('login')}>Login</button>
+          <button className="btn-register" onClick={() => setModalType('register')}>Cadastrar</button>
+        </div>
       </nav>
       
-      <main className="main-content">
+      <main className="main-content"> 
         <Outlet /> 
       </main>
+
+      {modalType && (
+        <AuthModal type={modalType}onClose={() => setModalType(null)}/>
+      )}
     </div>
   );
 }
